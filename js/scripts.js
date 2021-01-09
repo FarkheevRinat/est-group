9511976b80a191fd82433ee129e4acfc0b8b0e96
Farkheev Rinat <frinat04051985@gmail.com>
2021-01-09 16:23:54 +0500

    add adaptive all page

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', reviewsResize);
+window.addEventListener('resize', reviewsResize);
+
+function reviewsResize() {
+    const reviewsCounter = document.querySelector('.reviews__counter');
+    const reviewsImgBox = document.querySelector('.reviews__img-box');
+    const reviewsImgBoxHeight = reviewsImgBox.clientHeight;
+
+    const reviewsControls = document.querySelector('.reviews__controls');
+
+    if ( window.innerWidth <= 768 ) {
+        reviewsCounter.style.top = reviewsImgBoxHeight + 'px';
+        reviewsControls.style.top = reviewsImgBoxHeight / 2 + 'px';
+    } else {
+        reviewsCounter.style.top = 'auto';
+        reviewsControls.style.top = 'auto';
+    }
+}
